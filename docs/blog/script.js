84839c2673a2f28007fbce121cec179acc28d609
Dj <177370576+DJS4gnx@users.noreply.github.com>
2025-06-19 21:51:53 -0300

    edit footer

--- docs/blog/script.js
+++ docs/blog/script.js
@@ -17,6 +17,7 @@
 let blogName = "Jericho Tavern";
 let authorName = "DJSng";
 let authorLink = "https://djsng.neocities.org"; // Enter your website, social media, etc. Some way for people to tell you they like your blog! (Leaving it empty is okay too)
+let rssLink = "https://adamledoux.net/zoneRSS/"; // this makes life easier
 
 //-----------------------------
 
@@ -181,8 +182,7 @@ let headerHTML = '<ul> <li><a href="' + relativePath + '/index.html">Home</a></l
 '<li><a href="' + relativePath + '/about.html">About</a></li> </ul>';
 
 //Generate the Footer HTML, which uses the variables defined in the BASIC INFO section above to list info about the site.
-//Note: feel free to remove the references to Zonelets and Neocities! Just be careful not to delete any necessary HTML closing tags or other syntax.
-let footerHTML = "<hr><p>" + blogName + " by <a href='" + authorLink + "'>" + authorName + "</a>, built with <a href='https://zonelets.net/'>Zonelets</a>. Theme by <a href='https://kalechips.net/projects/layouts/'>kalechips</a>.</p>";
+let footerHTML = "<hr><p>" + "Jericho's Tavern" + " by <a href='" + authorLink + "'>" + authorName + "</a>. Powered by <a href='https://zonelets.net/'>Zonelets</a>, with theme by <a href='https://kalechips.net/projects/layouts/'>kalechips</a> and RSS feed powered by <a href='" + rssLink "''>zoneRSS</a></p>";
 
 //To do the following stuff, we want to know where we are in the posts array (if we're currently on a post page).
 let currentIndex = -1;
